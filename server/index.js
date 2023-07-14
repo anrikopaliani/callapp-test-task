094775161139c467a1f8e105bce401913c2da8b6
anrikopaliani <anrikopa@gmail.com>
2023-07-15 01:23:43 +0400

    chore:initialization

---
+++ server/index.js
@@ -0,0 +1,11 @@
+let express = require("express");
+
+let app = express();
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
