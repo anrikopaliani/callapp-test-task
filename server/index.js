293bbcb02f363bd55f8c73c485548d40c4580275
anrikopaliani <anrikopa@gmail.com>
2023-07-15 02:32:54 +0400

    feat: post request functionality

--- server/index.js
+++ server/index.js
@@ -2,11 +2,28 @@ let express = require("express");
 const fs = require("fs");
 let app = express();
 
-app.get("/", function (req, res) {
+app.use(express.json());
+
+app.get("/api/users", function (req, res) {
   fs.readFile("codeshare.json", (err, data) => {
     if (err) throw err;
     let users = JSON.parse(data);
-    res.send(users);
+    console.log(users);
+    res.json(users);
+  });
+});
+
+app.post("/api/users", function (req, res) {
+  fs.readFile("codeshare.json", (err, data) => {
+    if (err) throw err;
+    let users = JSON.parse(data);
+    users.push(req.body);
+    let newUsers = JSON.stringify(users);
+    fs.writeFile("codeshare.json", newUsers, (err) => {
+      if (err) throw err;
+
+      res.json(req.body);
+    });
   });
 });
 
